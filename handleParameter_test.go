2cbad76e6939d92462aefa78c9a98241005a7328
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-12-22 09:08:31 -0300

    go fomatter used

--- handleParameter_test.go
+++ handleParameter_test.go
@@ -6,82 +6,79 @@ import (
 	"testing"
 )
 
-func TestGetPort(t *testing.T){
-    t.Run("ManualPort", func(t *testing.T) {
-	expectedPort := "42069"
-	var expectedErr error = nil
+func TestGetPort(t *testing.T) {
+	t.Run("ManualPort", func(t *testing.T) {
+		expectedPort := "42069"
+		var expectedErr error = nil
 
-	got, err := GetPort("--PORT=42069")
+		got, err := GetPort("--PORT=42069")
 
-	if !reflect.DeepEqual(expectedPort, got) && expectedErr != err {
-	    t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected nil err but got '%s'", expectedPort, got, err.Error())
-	}
-    })
-    t.Run("LowerPort", func(t *testing.T){
-	expectedPort := ""
-	expectedErr := errors.New("Can't bind lower ports -- Received: 222")
+		if !reflect.DeepEqual(expectedPort, got) && expectedErr != err {
+			t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected nil err but got '%s'", expectedPort, got, err.Error())
+		}
+	})
+	t.Run("LowerPort", func(t *testing.T) {
+		expectedPort := ""
+		expectedErr := errors.New("Can't bind lower ports -- Received: 222")
 
-	got, err := GetPort("--PORT=222")
+		got, err := GetPort("--PORT=222")
 
-	if !reflect.DeepEqual(expectedPort, got) && expectedErr.Error() != err.Error() {
-	    t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected err to be '%s' but got '%s'", expectedPort, got, expectedErr.Error(), err.Error())
-	}
-    })
-    t.Run("HigherPort", func(t *testing.T){
-	expectedPort := ""
-	expectedErr := errors.New("Can't bind private ports -- Received: 69420")
+		if !reflect.DeepEqual(expectedPort, got) && expectedErr.Error() != err.Error() {
+			t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected err to be '%s' but got '%s'", expectedPort, got, expectedErr.Error(), err.Error())
+		}
+	})
+	t.Run("HigherPort", func(t *testing.T) {
+		expectedPort := ""
+		expectedErr := errors.New("Can't bind private ports -- Received: 69420")
 
-	got, err := GetPort("--PORT=69420")
+		got, err := GetPort("--PORT=69420")
 
-	if !reflect.DeepEqual(expectedPort, got) && expectedErr.Error() != err.Error() {
-	    t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected err: '%s' -- got: '%s'", expectedPort, got, expectedErr.Error(), err.Error())
-	}
-    })
+		if !reflect.DeepEqual(expectedPort, got) && expectedErr.Error() != err.Error() {
+			t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected err: '%s' -- got: '%s'", expectedPort, got, expectedErr.Error(), err.Error())
+		}
+	})
 
-    t.Run("InvalidPort", func(t *testing.T) {
-	expectedPort := ""
-	expectedErr := errors.New("The PORT must be a number ranging from 1,024 to 49,151, got: myPort")
+	t.Run("InvalidPort", func(t *testing.T) {
+		expectedPort := ""
+		expectedErr := errors.New("The PORT must be a number ranging from 1,024 to 49,151, got: myPort")
 
-	got, err := GetPort("--PORT=myPort")
+		got, err := GetPort("--PORT=myPort")
 
-	if !reflect.DeepEqual(expectedPort, got) && expectedErr.Error() != err.Error() {
-	    t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected err: '%s' -- got: '%s'", expectedPort, got, expectedErr.Error(), err.Error())
-	}
-    })
+		if !reflect.DeepEqual(expectedPort, got) && expectedErr.Error() != err.Error() {
+			t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected err: '%s' -- got: '%s'", expectedPort, got, expectedErr.Error(), err.Error())
+		}
+	})
 
-    t.Run("UnexpectedParam", func(t *testing.T) {
-	expectedPort := ""
-	expectedErr := errors.New("Unexpected parameter, expected --PORT=#### but received: --PART=2020")
+	t.Run("UnexpectedParam", func(t *testing.T) {
+		expectedPort := ""
+		expectedErr := errors.New("Unexpected parameter, expected --PORT=#### but received: --PART=2020")
 
-	got, err := GetPort("--PART=2020")
+		got, err := GetPort("--PART=2020")
 
-	if !reflect.DeepEqual(expectedPort, got) && expectedErr.Error() != err.Error() {
-	    t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected err: '%s' -- got '%s'", expectedPort, got, expectedErr.Error(), err.Error())
-	}
-    })
+		if !reflect.DeepEqual(expectedPort, got) && expectedErr.Error() != err.Error() {
+			t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected err: '%s' -- got '%s'", expectedPort, got, expectedErr.Error(), err.Error())
+		}
+	})
 
-    t.Run("MoreThanOneEqual", func(t *testing.T) {
-	expectedPort := ""
-	expectedErr := errors.New("Unexpected parameter, expected --PORT=#### but received: --PART=2020=")
+	t.Run("MoreThanOneEqual", func(t *testing.T) {
+		expectedPort := ""
+		expectedErr := errors.New("Unexpected parameter, expected --PORT=#### but received: --PART=2020=")
 
-	got, err := GetPort("--PORT=2020=")
+		got, err := GetPort("--PORT=2020=")
 
-	if !reflect.DeepEqual(expectedPort, got) && expectedErr.Error() != err.Error() {
-	    t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected err: '%s' -- got '%s'", expectedPort, got, expectedErr.Error(), err.Error())
-	}
-    })
+		if !reflect.DeepEqual(expectedPort, got) && expectedErr.Error() != err.Error() {
+			t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected err: '%s' -- got '%s'", expectedPort, got, expectedErr.Error(), err.Error())
+		}
+	})
 
-    t.Run("EmptyPort", func(t *testing.T) {
-	got, err := GetPort("--PoRt=")
+	t.Run("EmptyPort", func(t *testing.T) {
+		got, err := GetPort("--PoRt=")
 
-	expectedPort:=""
-	expectedErr:=errors.New("Empty PORT")
+		expectedPort := ""
+		expectedErr := errors.New("Empty PORT")
 
-	if !reflect.DeepEqual(expectedPort, got) && expectedErr.Error() != err.Error() {
-	    t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected err: '%s' -- got '%s'", expectedPort, got, expectedErr.Error(), err.Error())
-	}
-    })
+		if !reflect.DeepEqual(expectedPort, got) && expectedErr.Error() != err.Error() {
+			t.Errorf("Expected port: '%s' -- Got: '%s'\nExpected err: '%s' -- got '%s'", expectedPort, got, expectedErr.Error(), err.Error())
+		}
+	})
 }
-
-
-
--- handleReadme.go
+++ handleReadme.go
@@ -11,73 +11,73 @@ import (
 )
 
 type File struct {
-    path    string
-    content string
-    html    string
-    mu      sync.RWMutex
+	path    string
+	content string
+	html    string
+	mu      sync.RWMutex
 }
 
 func (f *File) watchReadme() {
-    watcher, err := fsnotify.NewWatcher()
-    if err != nil {
-	fmt.Printf("Failed creating the watcher: %s", err.Error())
-	return
-    }
-
-    defer watcher.Close()
-
-    go func(){
-	for {
-	    select{
-	    case event, ok := <-watcher.Events:
-		if !ok{
-		    return
-		}
-		log.Println("Event: ", event)
-		if event.Has(fsnotify.Rename){
-		    log.Println("README modified, updating content")
-		    f.updateReadmeContent()
-		}
-
-	    case err, ok := <-watcher.Errors:
-		if !ok{
-		    return
-		}
-		log.Printf("Watcher error: %s", err.Error())
-	    }
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Printf("Failed creating the watcher: %s", err.Error())
+		return
 	}
-    }()
 
-    err = watcher.Add(f.path)
-    if err != nil {
-	log.Printf("Failed adding filepath: %s", err.Error())
-	return
-    }
-    <-make(chan struct{})
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("Event: ", event)
+				if event.Has(fsnotify.Rename) {
+					log.Println("README modified, updating content")
+					f.updateReadmeContent()
+				}
+
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Printf("Watcher error: %s", err.Error())
+			}
+		}
+	}()
+
+	err = watcher.Add(f.path)
+	if err != nil {
+		log.Printf("Failed adding filepath: %s", err.Error())
+		return
+	}
+	<-make(chan struct{})
 }
 
 func (f *File) updateReadmeContent() {
-    data, err := os.ReadFile(f.path)
-    if err != nil {
-	log.Printf("Error reading f: %s", err.Error())
-	return
-    }
+	data, err := os.ReadFile(f.path)
+	if err != nil {
+		log.Printf("Error reading f: %s", err.Error())
+		return
+	}
 
-    f.mu.Lock()
-    f.content = string(data)
-    f.mu.Unlock()
+	f.mu.Lock()
+	f.content = string(data)
+	f.mu.Unlock()
 }
 
-func (f *File) serveReadme(w http.ResponseWriter, _ *http.Request){
-    f.mu.Lock()
-    defer f.mu.Unlock()
+func (f *File) serveReadme(w http.ResponseWriter, _ *http.Request) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
 
-    if f.content == "" {
-        http.Error(w, "File not found", http.StatusNotFound)
-        return
-    }
-    f.html = ConvertToHTML(f.content)
-    //TODO:CREATE HTML CONTENT WITH THE MARKDOWN
-    w.Header().Set("Content-Type", "text/html")
-    w.Write([]byte(f.html))
+	if f.content == "" {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
+	f.ToHTML()
+	//TODO:CREATE HTML CONTENT WITH THE MARKDOWN
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(f.html))
 }
