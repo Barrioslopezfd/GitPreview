4cd4e81bc071a92a20d8af9eee8b48a58cfcf96a
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-12-22 10:24:52 -0300

    add: default port and formatter

--- main.go
+++ main.go
@@ -7,37 +7,36 @@ import (
 	"os"
 )
 
-func main(){
+func main() {
 
-    if len(os.Args) > 2 {
-	log.Fatalf("Too many arguments, expected None or 1 -- Received: %d", len(os.Args))
-    }
-
-    var PORT string
-    var err error
-    if len(os.Args) == 2 {
-	PORT, err=GetPort(os.Args[1])
-	if err != nil {
-	    log.Fatal(err)
+	if len(os.Args) > 2 {
+		log.Fatalf("Too many arguments, expected None or 1 -- Received: %d", len(os.Args))
 	}
-    } else {
-	PORT = "8080"
-    }
 
-    var file *File
-    file = &File{}
-    file.path="./README.md"
+	var PORT string
+	var err error
+	if len(os.Args) == 2 {
+		PORT, err = GetPort(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		PORT = "8080"
+	}
 
-    file.updateReadmeContent()
+	var file *File
+	file = &File{}
+	file.path = "./README.md"
 
-    go file.watchReadme()
+	file.updateReadmeContent()
 
+	go file.watchReadme()
 
-    mux:=http.NewServeMux()
-    mux.HandleFunc("/", file.serveReadme)
-    server:=&http.Server{
-        Handler: mux,
-        Addr: fmt.Sprintf(":%s",PORT),
-    }
-    log.Fatal(server.ListenAndServe())
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", file.serveReadme)
+	server := &http.Server{
+		Handler: mux,
+		Addr:    fmt.Sprintf(":%s", PORT),
+	}
+	log.Fatal(server.ListenAndServe())
 }
